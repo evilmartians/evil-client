a9f08bb4b42f9f4a264a283f1c41d432ca10be0c
Andrew Kozin (aka nepalez) <andrew.kozin@gmail.com>
2015-10-22 23:12:03 +0300

    Implement Client's methods for building urn and uri

--- lib/evil/client.rb
+++ lib/evil/client.rb
@@ -15,12 +15,13 @@ module Evil
   #
   #    client.users[1].sms
   #
-  # Для получения строки адреса используется метод [#urn!]:
+  # Для получения строки адреса используются методы [#urn!] и [#uri!]:
   #
-  #    client.users[1].sms.urn! # => "127.0.0.1/v1/users/1/sms"
+  #    client.users[1].sms.urn! # => "users/1/sms"
+  #    client.users[1].sms.uri! # => "127.0.0.1/v1/users/1/sms"
   #
   # Методы [#get!], [#post!], [#patch!], [#delete!] формируют и отправляют
-  # запрос с переданными параметрами, возвращают (синхронный) ответ сервера
+  # синхронный запрос с переданными параметрами, возвращают ответ сервера
   # в виде +Hashie::Mash+.
   #
   #    response = client.users(1).sms.post! phone: "7101234567", text: "Hello!"
@@ -34,11 +35,11 @@ module Evil
   # При необходимости указать API явно, финализирующие методы могут вызываться
   # с его именем):
   #
-  #    client.users(1).urn! :users
-  #    # => "users.com/v1/users/1"
+  #    client.users(1).uri! :users
+  #    # => "www.users.com/v1/users/1"
   #
-  #    client.users(1).urn! :sms
-  #    # => "sms.com/v1/users/1"
+  #    client.users(1).uri! :sms
+  #    # => "www.sms.com/v1/users/1"
   #
   #    client.users(1).sms.post! :sms, phone: "7101234567", text: "Hello!"
   #
@@ -60,5 +61,54 @@ module Evil
     require_relative "client/api"
     require_relative "client/registry"
 
+    # Инициализирует объект клиента для единственной API
+    #
+    # @param [Hash] options Настройки API
+    # @options option (see Evil::Client::API#initialize)
+    #
+    # @return [Evil::Client]
+    #
+    def self.with(options)
+      new Registry.with(options)
+    end
+
+    # Возвращает текущий URN
+    #
+    # @return [String]
+    #
+    def urn! # метод без воскл.знака зарезервирован для части адреса
+      @urn.finalize!
+    end
+
+    # Находит нужный API и формирует для него URI из [#urn!]
+    #
+    # @param [Array<Symbol>] api_keys
+    #   Имена API среди которых ведется поиск URN (по умолчанию - по всем URN)
+    #
+    # @return [<type>] <description>
+    #
+    def uri!(*api_keys)
+      urn = urn!
+      @registry.api(*api_keys, urn: urn).uri(urn)
+    end
+
+    private
+
+    def initialize(registry)
+      @registry = registry # коллекция удаленных API
+      @urn = URN           # ленивый построитель URN
+    end
+
+    def update!(&block)
+      dup.tap { |client| client.instance_eval(&block) }
+    end
+
+    def method_missing(name, *args)
+      update! { @urn = @urn.public_send(name, *args) }
+    end
+
+    def respond_to_missing?(name, *)
+      @urn.respond_to?(name)
+    end
   end
 end
---
+++ spec/unit/client_spec.rb
@@ -0,0 +1,76 @@
+describe Evil::Client do
+
+  describe ".new" do
+    subject { described_class.new double(:registry) }
+
+    # just check it isn't fail because all its variables are hidded
+    it { is_expected.to be_kind_of described_class }
+  end
+
+  let(:client) { described_class.with base_url: "http://localhost/v1" }
+
+  describe ".with" do
+    subject { client }
+
+    it "instantiates the client" do
+      expect(subject).to be_kind_of described_class
+    end
+
+    it "sets base_url to the only used API" do
+      expect(subject.uri!).to eql "http://localhost/v1/"
+    end
+  end
+
+  describe "#urn!" do
+    it "returns empty string by default" do
+      expect(client.urn!).to eql ""
+    end
+
+    it "returns current urn after modification" do
+      expect(client.foo[1].bar.urn!).to eql "foo/1/bar"
+    end
+  end
+
+  describe "#uri!" do
+    it "returns base_url by default" do
+      expect(client.uri!).to eql "http://localhost/v1/"
+    end
+
+    it "returns current uri after modification" do
+      expect(client.foo[1].bar.uri!)
+        .to eql "http://localhost/v1/foo/1/bar"
+    end
+
+    it "uses api keys to restrict APIs" do
+      expect { client.foo[1].bar.uri! :wrong }
+        .to raise_error Evil::Client::Errors::URNError, %r{\:wrong.+'foo/1/bar'}
+    end
+  end
+
+  describe "#method_missing" do
+    subject { client.foo[1].bar }
+
+    it "returns new client instance" do
+      expect(subject).to be_kind_of described_class
+      expect(subject).not_to eql client
+    end
+
+    it "updates urn!" do
+      expect(subject.urn!).to eql "foo/1/bar"
+    end
+
+    it "behaves by default for methods with bang" do
+      expect { client.foo! }.to raise_error NoMethodError
+    end
+  end
+
+  describe "#respond_to_missing?" do
+    it "returns true for methods without bang" do
+      expect(client).to respond_to :arbitrary_name
+    end
+
+    it "returns false for methods with bang" do
+      expect(client).not_to respond_to :arbitrary_name!
+    end
+  end
+end
