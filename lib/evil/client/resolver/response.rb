f6abe17ed74e4857af038e0f95ee36847d847a58
Andrey Novikov <envek@envek.name>
2017-10-25 00:31:35 +0300

    Allow to pass response handling to parent scopes
    For example to allow to handle specific cases in operation handlers
    and common cases – in parent scopes.

--- lib/evil/client/resolver/response.rb
+++ lib/evil/client/resolver/response.rb
@@ -6,6 +6,9 @@ class Evil::Client
   class Resolver::Response < Resolver
     private
 
+    PROCESSING_DONE = Object.new
+    SKIP_RESPONSE   = Object.new
+
     def initialize(schema, settings, response)
       @__response__ = Array response
       super schema, settings, :responses, @__response__.first.to_i
@@ -13,14 +16,20 @@ class Evil::Client
 
     def __call__
       super do
-        __check_status__
-        instance_exec(*@__response__, &__blocks__.last)
+        catch(PROCESSING_DONE) do
+          __blocks__.reverse_each do |block|
+            catch(SKIP_RESPONSE) do
+              throw(PROCESSING_DONE, instance_exec(*@__response__, &block))
+            end
+          end
+          # We're here if 1) no blocks or 2) all blocks skipped processing
+          raise ResponseError.new(@__schema__, @__settings__, @__response__)
+        end
       end
     end
 
-    def __check_status__
-      return if __blocks__.any?
-      raise ResponseError.new(@__schema__, @__settings__, @__response__)
+    def super!
+      throw SKIP_RESPONSE
     end
   end
 end
