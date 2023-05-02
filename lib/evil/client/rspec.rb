a823d7f9d4ccbbb87540df2d2b265c73649845ed
Pasha Kalashnikov <kalashnikovisme@gmail.com>
2023-05-02 20:33:35 +0400

    Fix rspec definition

--- lib/evil/client/rspec.rb
+++ lib/evil/client/rspec.rb
@@ -1,25 +1,27 @@
-class Evil::Client
-  #
-  # Collection of RSpec-related definitions
-  #
-  module RSpec
-    require_relative "rspec/evil_client_schema_matching"
-    require_relative "rspec/base_stub"
-    require_relative "rspec/allow_stub"
-    require_relative "rspec/expect_stub"
+module Evil
+  class Client
+    #
+    # Collection of RSpec-related definitions
+    #
+    module RSpec
+      require_relative "rspec/evil_client_schema_matching"
+      require_relative "rspec/base_stub"
+      require_relative "rspec/allow_stub"
+      require_relative "rspec/expect_stub"
 
-    def stub_client_operation(klass = Evil::Client, name = nil)
-      AllowStub.new(klass, name)
-    end
+      def stub_client_operation(klass = Evil::Client, name = nil)
+        AllowStub.new(klass, name)
+      end
 
-    def expect_client_operation(klass, name = nil)
-      ExpectStub.new(klass, name)
-    end
+      def expect_client_operation(klass, name = nil)
+        ExpectStub.new(klass, name)
+      end
 
-    def unstub_all
-      allow(Evil::Client::Container::Operation)
-        .to receive(:new)
-        .and_call_original
+      def unstub_all
+        allow(Evil::Client::Container::Operation)
+          .to receive(:new)
+          .and_call_original
+      end
     end
   end
 end
